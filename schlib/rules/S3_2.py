e3a8d94b30864a07aa5cde6d4e9679ad00b71d51
Carsten Presser <c@rstenpresser.de>
2020-10-18 17:53:16 +0200

    Rework S3.2 check for v6

--- schlib/rules/S3_2.py
+++ schlib/rules/S3_2.py
@@ -7,29 +7,24 @@ class Rule(KLCRule):
     """
     Create the methods check and fix to use with the kicad lib files.
     """
+    v6 = True
     def __init__(self, component):
-        super(Rule, self).__init__(component, 'Text fields should use common size of 50mils, but labels and numbers may use text size as low as 20mil if required')
+        super(Rule, self).__init__(component, 'Text properties should use common size of 50mils, but labels and numbers may use text size as low as 20mil if required')
 
     def check(self):
         """
         Proceeds the checking of the rule.
         The following variables will be accessible after checking:
             * violating_pins
-            * violating_fields
+            * violating_properties
         """
-        self.violating_fields = []
-        for field in self.component.fields:
-            text_size = int(field['text_size'])
+        self.violating_properties = []
+        for prop in self.component.properties:
+            text_size = self.mm_to_mil(prop.effects.sizex)
             if (text_size != 50):
-                self.violating_fields.append(field)
-                if "reference" in field.keys():
-                    message = field["reference"][1:-1]
-                elif len(field["name"]) > 2:
-                    message = field["name"][1:-1]
-                else:
-                    message = "UNKNOWN"
-                message += (" at posx {0} posy {1}".format(field["posx"], field["posy"]))
-                self.error(" - Field {0} size {1}".format(message, field["text_size"]))
+                self.violating_properties.append(prop)
+                message = ("{0} at posx {1} posy {2}".format(prop.name, self.mm_to_mil(prop.posx), self.mm_to_mil(prop.posy)))
+                self.error(" - Field {0} size {1}".format(message, text_size))
 
         self.violating_pins = []
 
@@ -40,19 +35,19 @@ class Rule(KLCRule):
         """
 
         for pin in self.component.pins:
-            name_text_size = int(pin['name_text_size'])
-            num_text_size = int(pin['num_text_size'])
+            name_text_size = self.mm_to_mil(pin.name_effect.sizex)
+            num_text_size = self.mm_to_mil(pin.number_effect.sizex)
 
             if (name_text_size < 20) or (name_text_size > 50) or (num_text_size < 20) or (num_text_size > 50):
                 self.violating_pins.append(pin)
-                self.error(' - Pin {0} ({1}), text size {2}, number size {3}'.format(pin['name'], pin['num'], pin['name_text_size'], pin['num_text_size']))
+                self.error(' - Pin {0} ({1}), text size {2}, number size {3}'.format(pin.name, pin.number, name_text_size, num_text_size))
             else:
                 if name_text_size != 50:
-                    self.warning("Pin name text size should be 50mils (or 20...50mils if required by the symbol geometry)")
+                    self.warning("Pin {0} ({1}) name text size should be 50mils (or 20...50mils if required by the symbol geometry)".format(pin.name, pin.number))
                 if num_text_size != 50:
-                    self.warning("Pin number text size should be 50mils (or 20...50mils if required by the symbol geometry)")
+                    self.warning("Pin {0} ({1}) number text size should be 50mils (or 20...50mils if required by the symbol geometry)".format(pin.name, pin.number))
 
-        if (len(self.violating_fields) > 0 or
+        if (len(self.violating_properties) > 0 or
             len(self.violating_pins) > 0):
             return True
 
@@ -62,16 +57,17 @@ class Rule(KLCRule):
         """
         Proceeds the fixing of the rule, if possible.
         """
-        if len(self.violating_fields) > 0:
+        if len(self.violating_properties) > 0:
             self.info("Fixing field text size")
-        for field in self.violating_fields:
-            field['text_size'] = '50'
+        for prop in self.violating_properties:
+            prop.effects.sizex = self.mil_to_mm(50)
+            prop.effects.sizey = self.mil_to_mm(50)
 
         if len(self.violating_pins) > 0:
             self.info("Fixing pin text size")
         for pin in self.violating_pins:
-            pin['name_text_size'] = '50'
-            pin['num_text_size'] = '50'
+            pin.name_effect.sizex = self.mil_to_mm(50)
+            pin.name_effect.sizey = self.mil_to_mm(50)
+            pin.number_effect.sizex = self.mil_to_mm(50)
+            pin.number_effect.sizey = self.mil_to_mm(50)
         self.recheck()
-
-
