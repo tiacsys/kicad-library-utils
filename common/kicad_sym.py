c7b6dd2c4c10b152321c961ee54f3ba0f2c1b71e
cpresser <c@rstenpresser.de>
2022-02-08 19:40:37 +0000

    Exclude filename of KicadSymbol from __eq__

--- common/kicad_sym.py
+++ common/kicad_sym.py
@@ -635,7 +635,7 @@ class Property(KicadSymbolBase):
 class KicadSymbol(KicadSymbolBase):
     name: str
     libname: str
-    filename: str
+    filename: str = field(compare=False)
     properties: List[Property] = field(default_factory=list)
     pins: List[Pin] = field(default_factory=list)
     rectangles: List[Rectangle] = field(default_factory=list)
