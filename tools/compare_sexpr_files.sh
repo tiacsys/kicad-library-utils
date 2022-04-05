88a66a9dd4c7d9c3b67fd30900b703dc15dc245b
Lars Kruse <devel@sumpfralle.de>
2022-04-05 04:26:52 +0200

    refactor(compare_sexpr_files): improve style and remove bash-dependency
    * remove bash-specific features (nothing special as used)
        * bash is not installed by default anymore (in Debian: more than 10
          years)
    * temporary files are removed even in case of error
    * shorter (but still readable) embedded python code
    * allow to read one of the two files from stdin
    * strict execution (exit-on-unset, exit-on-error)

--- tools/compare_sexpr_files.sh
+++ tools/compare_sexpr_files.sh
@@ -1,42 +1,40 @@
-#!/usr/bin/env bash
-
+#!/bin/sh
+#
 # This script file is used to compare to sexpr files
 # with sexpr python file. Spaces and line positioning will be ignored
+#
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 fileA fileB"
+set -eu
+
+
+if [ $# -ne 1 ] && [ $# -ne 2 ]; then
+    echo >&2 "Usage: $0 fileA [fileB]"
     exit 1
 fi
 
+INPUT_FILE1="$1"
+# the second file may be omitted - in this case we read from stdin
+INPUT_FILE2="${2:-}"
 
-function normalize_sexpr {
-# python code --- START
-PYTHONPATH="$(dirname "$0")/../common" python3 << EOF
-import sexpr, re
 
-# read file
-f = open('$1')
-data = ''.join(f.readlines())
-out = sexpr.format_sexp(sexpr.build_sexp(sexpr.parse_sexp(data)))
-
-# write file
-f = open('$2', 'w')
-f.write(out)
-f.close()
-EOF
-# python code --- END
+# generate the canonical representation of a sexpr file (read from stdin, write to stdout)
+normalize_sexpr() {
+    PYTHONPATH="$(dirname "$0")/../common" python3 -c '
+import sexpr, sys
+print(sexpr.format_sexp(sexpr.build_sexp(sexpr.parse_sexp(sys.stdin.read()))))
+'
 }
+
 
 o1="$(mktemp)"
 o2="$(mktemp)"
-args=("$@")
-normalize_sexpr ${args[0]} $o1
-normalize_sexpr ${args[1]} $o2
+
+# remove the files when the script exits somehow
+cleanup() { rm -f "$o1" "$o2"; }
+trap cleanup EXIT
+
+normalize_sexpr <"$INPUT_FILE1" >"$o1"
+if [ -z "$INPUT_FILE2" ]; then cat; else cat "$INPUT_FILE2"; fi | normalize_sexpr >"$o2"
 
 # use git compare since it does a nice colorful output
-git --no-pager diff --no-index --color-words $o1 $o2
-R=$?
-
-# remove temp files, forward exit code
-rm $o1 $o2
-exit $R
+git --no-pager diff --no-index --color-words "$o1" "$o2"
