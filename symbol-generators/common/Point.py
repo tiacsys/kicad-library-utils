d05ce1a60f80a10c75782840e018dbff5f6aa9ef
Dominik Wernberger <dominik2.wernberger@tum.de>
2022-04-10 18:02:09 +0200

    Revert `self` for `roundCoordinateToGrid`

--- symbol-generators/common/Point.py
+++ symbol-generators/common/Point.py
@@ -94,7 +94,7 @@ class Point():
             point.roundToGrid()
         return point
 
-    def roundCoordinateToGrid(self, value: float, base: int, apply_on_copy: bool = False):
+    def roundCoordinateToGrid(value: float, base: int, apply_on_copy: bool = False):
         if value >= 0:
             return math.floor(value / base) * base
         return math.ceil(value / base) * base
