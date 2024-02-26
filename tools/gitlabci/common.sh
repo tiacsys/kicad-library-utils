563a0f45bc13939017f1f7e6d3b46d78bae9e0bb
Carsten Presser <c@rstenpresser.de>
2024-02-26 14:03:55 +0100

    Fix URL of the 'View App' button in MRs
    Commit edeb00a5827cade4639020f0684262c3114900e2 did not fix this, instead it made
    the button not work at all.
    
    Unfortunately it is not as simple as one hopes to build the URL. There are two
    distinct cases that need different handling.

--- tools/gitlabci/common.sh
+++ tools/gitlabci/common.sh
@@ -3,3 +3,16 @@
 API_RESPONSE=$(curl -s -H "JOB_TOKEN: $CI_JOB_TOKEN" "https://gitlab.com/api/v4/projects/$CI_MERGE_REQUEST_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID")
 BASE_SHA=$( echo $API_RESPONSE | python3 -c "import sys, json; print (json.load(sys.stdin)['diff_refs']['base_sha'])")
 TARGET_SHA=$( echo $API_RESPONSE | python3 -c "import sys, json; print (json.load(sys.stdin)['diff_refs']['head_sha'])")
+
+
+
+# unfortunately the URL where the assets are deployed differs depending on who runs the pipeline
+# there are two cases, the CI can run in the project context, or in the contributors context
+# we use the schema for a 'dynamic' URL from the manual
+# https://docs.gitlab.com/ee/ci/environments/#set-a-dynamic-environment-url
+if [ $CI_PROJECT_ROOT_NAMESPACE == "kicad" ]; then
+  DYNAMIC_ENVIRONMENT_URL="https://$CI_PROJECT_ROOT_NAMESPACE.$CI_PAGES_DOMAIN/-/libraries/$CI_PROJECT_NAME/-/jobs/$CI_JOB_ID/artifacts/diffs/index.html.diff/index.html"
+else
+  DYNAMIC_ENVIRONMENT_URL="https://$CI_PROJECT_ROOT_NAMESPACE.$CI_PAGES_DOMAIN/-/$CI_PROJECT_NAME/-/jobs/$CI_JOB_ID/artifacts/diffs/index.html.diff/index.html"
+fi
+echo "DYNAMIC_ENVIRONMENT_URL=$DYNAMIC_ENVIRONMENT_URL" >> deploy.env
