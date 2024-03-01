614e27eb76780ee064204c7a7a2c034d9b2ec464
kliment <kliment@0xfb.com>
2024-03-01 20:34:02 +0000

    screenshot task: Pass full path to ImportFootprint
    See merge request kicad/libraries/kicad-library-utils!526

--- scripts/create_board.py
+++ scripts/create_board.py
@@ -24,8 +24,7 @@ fp_path = Path(os.path.abspath(args.footprint))
 
 board = pcbnew.BOARD()
 io = pcbnew.PCB_IO_KICAD_SEXPR()
-fp: pcbnew.FOOTPRINT = io.ImportFootprint(str(fp_path.parent), fp_path.stem)
-
+fp: pcbnew.FOOTPRINT = io.ImportFootprint(str(fp_path), fp_path.stem)
 board.Add(fp)
 # put the footprint at the center-ish, away from the page borders
 fp.SetPosition(pcbnew.VECTOR2I_MM(150, 100))
