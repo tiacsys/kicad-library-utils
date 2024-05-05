a704d4544237f67be756764c55562c3c4ca8049f
Uli Köhler <ukoehler@techoverflow.net>
2024-05-05 17:19:45 +0200

    Add kicad-cli sym upgrade --force check

--- tools/gitlabci/check_symbols.sh
+++ tools/gitlabci/check_symbols.sh
@@ -42,6 +42,43 @@ LIBS_OLD=$(git diff-tree --diff-filter=DMR --no-commit-id --oneline --name-only 
 echo "Found new Libraries: $LIBS_NEW"
 echo "Found old Libraries: $LIBS_OLD"
 
+LIB_UPGRADE_ERROR_COUNT=$?
+
+# Perform kicad-cli sym upgrade --force on each library
+for LIBNAME in $LIBS_NEW; do
+  echo "Upgrading $LIBNAME"
+  # Perform sha256sum before
+  sha256sum "$CI_PROJECT_DIR/$LIBNAME"
+
+  $KICAD_CLI --upgrade --sym-lib "$CI_PROJECT_DIR/$LIBNAME" --force
+
+  # Perform sha256sum after
+  sha256sum_before=$(sha256sum "$CI_PROJECT_DIR/$LIBNAME" | awk '{print $1}')
+  $KICAD_CLI --upgrade --sym-lib "$CI_PROJECT_DIR/$LIBNAME" --force > /dev/null
+  upgrade_status=$?
+  sha256sum_after=$(sha256sum "$CI_PROJECT_DIR/$LIBNAME" | awk '{print $1}')
+
+  if [ $upgrade_status -ne 0 ]; then
+    echo "ERROR: 'kicad-cli sym upgrade --force $LIBNAME' failed (exit code $upgrade_status)."
+    echo "This means the library can't be loaded in KiCad."
+    echo "This might be caused by a derived symbol appearing before the base symbol in the file."
+    # Fail only later so we can check all libraries
+    LIB_UPGRADE_ERROR_COUNT=$(($LIB_UPGRADE_ERROR_COUNT + 1))
+  elif [ "$sha256sum_before" != "$sha256sum_after" ]; then
+    echo "ERROR: 'kicad-cli sym upgrade --force' changes $LIBNAME ."
+    echo "Please re-save in KiCad or run 'kicad-cli sym upgrade --force $LIBNAME'"
+    # Fail only later so we can check all libraries
+    LIB_UPGRADE_ERROR_COUNT=$(($LIB_UPGRADE_ERROR_COUNT + 1))
+  fi
+done
+
+# NOTE: We should not perform klc-check here since the library are broken or modified
+# by the kicad-cli sym upgrade --force call.
+if [ $LIB_UPGRADE_ERROR_COUNT -ne 0 ]; then
+  echo "Fatal error: $LIB_UPGRADE_ERROR_COUNT libraries failed integrity check"
+  exit $LIB_UPGRADE_ERROR_COUNT
+fi
+
 # checkout the previous version of the 'old' files so we can compare against them
 mkdir -p $CI_BUILDS_DIR/kicad-symbols-prev
 for LIBNAME in $LIBS_OLD; do
