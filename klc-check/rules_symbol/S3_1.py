b56880c30477a7c03d979ae4f363820ef46a38d3
John Beard <john.j.beard@gmail.com>
2024-10-01 14:23:54 +0000

    KLC check: S3.1: allow 'small' (sub-800mil) symbols to be on a 50mil centre
    See merge request kicad/libraries/kicad-library-utils!548

--- klc-check/rules_symbol/S3_1.py
+++ klc-check/rules_symbol/S3_1.py
@@ -26,6 +26,9 @@ class Rule(KLCRule):
             center_pl = self.component.get_center_rectangle([0, unit])
             if center_pl is not None:
                 (x, y) = center_pl.get_center_of_boundingbox()
+
+                bbox = center_pl.get_boundingbox()
+                size = [bbox[0] - bbox[2], bbox[1] - bbox[3]]
             else:
                 pins = [pin for pin in self.component.pins if (pin.unit in [unit, 0])]
 
@@ -44,20 +47,44 @@ class Rule(KLCRule):
                 x = (x_min + x_max) / 2
                 y = (y_min + y_max) / 2
 
+                size = [x_max - x_min, y_max - y_min]
+
             # convert to mil
             x = mm_to_mil(x)
             y = mm_to_mil(y)
+
+            size = [mm_to_mil(size[0]), mm_to_mil(size[1])]
+
             # Right on the middle!
             if x == 0 and y == 0:
                 continue
-            elif math.fabs(x) <= 50 and math.fabs(y) <= 50:
-                self.warning("Symbol unit {unit} slightly off-center".format(unit=unit))
-                self.warningExtra("  Center calculated @ ({x}, {y})".format(x=x, y=y))
+            elif math.fabs(x) == 50 or math.fabs(y) == 50:
+                # Sometimes the symbol is slightly off-center, but it's ok
+
+                def is_bad_offset(axis_offset, axis_size):
+                    """
+                    An offset is acceptable is it's exactly 50 AND the symbol is "small"
+                    """
+                    # Symbols smaller than this can be 50 mil off-centre
+                    # e.g. for symbols with a even number of 100 mil-spaced pin
+                    # The right exact value is subjective.
+                    small_symbol_size = 800
+
+                    # Symbol is big enough to need proper centering
+                    if axis_size > small_symbol_size:
+                        return math.fabs(axis_offset) != 0
+
+                    # Symbol is small enough to allow 50 mil off-center
+                    return math.fabs(axis_offset) not in [0, 50]
+
+                if is_bad_offset(x, size[0]) or is_bad_offset(y, size[1]):
+                    self.warning(f"Symbol unit {unit} slightly off-center")
+                    self.warningExtra(f"  Center calculated @ ({x}, {y})")
             else:
                 self.error(
-                    "Symbol unit {unit} not centered on origin".format(unit=unit)
+                    f"Symbol unit {unit} not centered on origin"
                 )
-                self.errorExtra("Center calculated @ ({x}, {y})".format(x=x, y=y))
+                self.errorExtra(f"Center calculated @ ({x}, {y})")
 
         return False
 
