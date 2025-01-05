a2f69232b8d896d143519800c1669474d1554509
John Beard <john.j.beard@gmail.com>
2025-01-05 13:31:24 +0000

    CI: add a bit of debug for return codes
    See merge request kicad/libraries/kicad-library-utils!598

--- tools/gitlabci/check_footprints.sh
+++ tools/gitlabci/check_footprints.sh
@@ -30,17 +30,24 @@ $CI_BUILDS_DIR/kicad-library-utils/klc-check/check_lib_table.py $CI_PROJECT_DIR/
 
 TABLE_ERRORS=$?
 
+echo "KLC_ERRORS: $KLC_ERRORS"
+echo "TABLE_ERRORS: $TABLE_ERRORS"
+
 # Table errors are always bad
 if [ $TABLE_ERRORS -ne 0 ]; then
+    echo "Library table errors"
     exit 1
 fi
 
 if [ $KLC_ERRORS -eq 2 ]; then
     # Warnings
+    echo "KLC checks have warnings only"
     exit 2
 elif [ $KLC_ERRORS -ne 0 ]; then
     # Errors (or something else, assume bad)
+    echo "KLC checks have errors"
     exit 1
 fi
 
+echo "All checks passed"
 exit 0
