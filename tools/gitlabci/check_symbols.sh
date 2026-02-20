9d490a9207952fdebae41ce1321a077dc73bc2e7
Carsten Presser <c@rstenpresser.de>
2026-02-20 18:52:04 +0100

    CI: Remove CI_BUILDS_DIR from tools/gitlabci/check_symbols.sh
    The check_symbols.sh script used to clone a footprint repo and extract old
    versions of symbols from git. Instead of storing them in CI_BUILDS_DIR they
    are now stored in a temporary directory. In a CI context that does not need
    to get cleaned up because the container will get thrown away.
    
    Related: https://gitlab.com/kicad/libraries/kicad-library-utils/-/issues/404O

--- tools/gitlabci/check_symbols.sh
+++ tools/gitlabci/check_symbols.sh
@@ -4,8 +4,8 @@
 # Inside the kicad-symbols folder, run
 #     export BASE_SHA=$(git rev-parse master)
 #     export TARGET_SHA=$(git rev-parse HEAD)
-#     export CI_PROJECT_DIR=$(pwd)
-#     export CI_BUILDS_DIR=..
+#     export KICAD_LIBRARY_UTILS_DIR=$(pwd)
+#     export TMP_BUILD_DIR=`mktemp -d` # or supply any temp directory
 #     ../kicad-library-utils/tools/gitlabci/check_symbols.sh
 
 source $(dirname ${BASH_SOURCE[0]})/common.sh
@@ -18,8 +18,8 @@ if [ -z "$CI_PROJECT_DIR" ]; then
   echo "CI_PROJECT_DIR is not set"
   exit 1
 fi
-if [ -z "$CI_BUILDS_DIR" ]; then
-  echo "CI_BUILDS_DIR is not set"
+if [ -z "$KICAD_LIBRARY_UTILS_DIR" ]; then
+  echo "KICAD_LIBRARY_UTILS_DIR is not set"
   exit 1
 fi
 if [ -z "$BASE_SHA" ]; then
@@ -30,16 +30,22 @@ if [ -z "$TARGET_SHA" ]; then
   echo "TARGET_SHA is not set"
   exit 1
 fi
+if [ -z "$TMP_BUILD_DIR" ]; then
+  echo "TMP_BUILD_DIR is not set, creating a temp dir. You might want to delete it later."
+  TMP_BUILD_DIR=`mktemp -d`
+fi
+
 
 echo "BASE_SHA: $BASE_SHA"
 echo "TARGET_SHA: $TARGET_SHA"
 echo "CI_PROJECT_DIR: $CI_PROJECT_DIR"
-echo "CI_BUILDS_DIR: $CI_BUILDS_DIR"
+echo "TMP_BUILD_DIR: $TMP_BUILD_DIR"
+echo "KICAD_LIBRARY_UTILS_DIR: $KICAD_LIBRARY_UTILS_DIR"
 
 # clone required repos
-if [ ! -x "$CI_BUILDS_DIR/kicad-footprints" ]; then
+if [ ! -x "$TMP_BUILD_DIR/kicad-footprints" ]; then
   echo "Cloning footprint repo"
-  git clone --depth 1 https://gitlab.com/kicad/libraries/kicad-footprints.git $CI_BUILDS_DIR/kicad-footprints
+  git clone --depth 1 https://gitlab.com/kicad/libraries/kicad-footprints.git $TMP_BUILD_DIR/kicad-footprints
 else
   echo "Found existing kicad-footprints directory"
 fi
@@ -53,9 +59,9 @@ echo "Found changed Symbols: $CHANGED_SYMBOLS"
 echo "Found deleted Symbols: $DELETED_SYMBOLS"
 
 # checkout the previous version of the 'old' files so we can compare against them
-mkdir -p $CI_BUILDS_DIR/kicad-symbols-prev
+mkdir -p $TMP_BUILD_DIR/kicad-symbols-prev
 for LIBNAME in $CHANGED_SYMBOLS; do
-  TARGET_FILENAME="$CI_BUILDS_DIR/kicad-symbols-prev/$LIBNAME"
+  TARGET_FILENAME="$TMP_BUILD_DIR/kicad-symbols-prev/$LIBNAME"
   # the LIBAME will look like "Video.kicad_symdir/SI582.kicad_sym".
   # So we need to create the directory Video.kicad_symdir first
   # we do this hacky, my creating it with mkdir -p and then deleting the 
@@ -63,15 +69,15 @@ for LIBNAME in $CHANGED_SYMBOLS; do
   mkdir -p "$TARGET_FILENAME"
   rmdir "$TARGET_FILENAME"
   # now extract the old version of the changed library from git
-  git cat-file blob "$BASE_SHA:$LIBNAME" > "$CI_BUILDS_DIR/kicad-symbols-prev/$LIBNAME"
+  git cat-file blob "$BASE_SHA:$LIBNAME" > "$TMP_BUILD_DIR/kicad-symbols-prev/$LIBNAME"
 done
 
 # now run comparelibs
 $KICAD_LIBRARY_UTILS_DIR/klc-check/comparelibs.py -v \
-  --old $CI_BUILDS_DIR/kicad-symbols-prev/* \
+  --old $TMP_BUILD_DIR/kicad-symbols-prev/* \
   --new $CHANGED_SYMBOLS \
   --check --check-derived \
-  --footprint_directory $CI_BUILDS_DIR/kicad-footprints \
+  --footprint_directory $TMP_BUILD_DIR/kicad-footprints \
   -m \
   --junit junit.xml
 KLC_ERRORS=$?
