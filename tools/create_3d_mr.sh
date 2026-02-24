865614c3b71f6cc3adc11f7548e488279b477f3b
kliment <kliment@0xfb.com>
2026-02-24 10:10:05 +0000

    CI: Fix 3d-auto-mr to have better variable name for temp dir
    Also skip removal of files at end. The container gets destroyed immediately after that step,
    so no need for extra CPU cycles.
    
    See merge request https://gitlab.com/kicad/libraries/kicad-library-utils/-/merge_requests/757

--- tools/create_3d_mr.sh
+++ tools/create_3d_mr.sh
@@ -2,14 +2,14 @@
 
 source $KICAD_LIBRARY_UTILS_DIR/tools/gitlabci/common.sh
 
-export BUILDTARGET=`realpath "$TMP_BUILD_DIR"`
+export TMP_BUILD_DIR=`mktemp -d`
 echo "Cloning 3d model repo"
-git clone "${KICAD_MODELS_REPO}" "$BUILDTARGET/models" --branch master
+git clone "${KICAD_MODELS_REPO}" "$TMP_BUILD_DIR/models" --branch master
 cd src/generators
-echo "/generate_changed_models.py -p ${BASE_SHA} -c ${TARGET_SHA} -o ${BUILDTARGET}/models -i -v"
-`dirname $0`/generate_changed_models.py -p "${BASE_SHA}" -c "${TARGET_SHA}" -o "${BUILDTARGET}/models" -i -v
+echo "/generate_changed_models.py -p ${BASE_SHA} -c ${TARGET_SHA} -o ${TMP_BUILD_DIR}/models -i -v"
+`dirname $0`/generate_changed_models.py -p "${BASE_SHA}" -c "${TARGET_SHA}" -o "${TMP_BUILD_DIR}/models" -i -v
 export branchname="gen-mr-${CI_MERGE_REQUEST_IID}"
-cd "$BUILDTARGET/models"
+cd "$TMP_BUILD_DIR/models"
 git switch -c "$branchname"
 git status
 git config --global user.email "kicad-mr-bot@example.com"
@@ -20,4 +20,3 @@ git push -uf "https://gitlab-ci-token:${MR_3D_TOKEN}@gitlab.com/kicad/libraries/
 export MR_DESCRIPTION="Output of merge request ${CI_MERGE_REQUEST_PROJECT_URL}/-/merge_requests/${CI_MERGE_REQUEST_IID} ${CI_MERGE_REQUEST_TITLE}"
 export MR_TITLE="Generator MR ${CI_MERGE_REQUEST_IID} ${CI_MERGE_REQUEST_TITLE}"
 gitlab --private-token "${MR_3D_TOKEN}" project-merge-request create --project-id 68922623 --source-branch "$branchname" --target-branch master --allow-collaboration true --squash true --remove-source-branch true --target-project-id 21604637 --title "$MR_TITLE" --description "$MR_DESCRIPTION"
-rm -rf $TMP_BUILD_DIR/*
